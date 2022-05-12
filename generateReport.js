74d0226aed3e8b6b7170f97090d3d1f241101023
Anthony Gauthier <antho325@hotmail.com>
2022-05-12 10:20:20 -0400

    feat: added generateReport.js, added steps in action.yaml for reporting

---
+++ generateReport.js
@@ -0,0 +1,21 @@
+import * as core from '@actions/core' 
+
+const generateTestResults = () => {
+
+}
+
+console.log('foo')
+
+// await core.summary
+//     .addHeading('Gatling Test Results')
+//     .addCodeBlock(generateTestResults(), "js")
+//     .addTable([
+//         [{data: 'Request', header: true}, {data: 'p90', header: true}],
+//         ['foo.js', 'Pass ✅'],
+//         ['bar.js', 'Fail ❌'],
+//         ['test.js', 'Pass ✅']
+//     ])
+//     .addLink('View staging deployment!', 'https://github.com')
+//     .write()
+
+
