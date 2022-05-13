cd7237a08a892c03375579aacdbc055dc66adf19
Anthony Gauthier <antho325@hotmail.com>
2022-05-12 22:17:04 -0400

    feat: attempt at writing summary

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
 const core = require('@actions/core');
 const fs   = require('fs');
 
+// const filePath = `./test/target`
+const filePath = '/Users/agauthier/Documents/Personal-GH/liatrio-gatling-test-showcase/target'
+
 const generateTestResults = (runName) => {
-    const results = JSON.parse(fs.readFileSync(`./test/target/gatling/${runName}/js/stats.json`).toString());
+    const results = JSON.parse(fs.readFileSync(`${filePath}/gatling/${runName}/js/stats.json`).toString());
     
-    for(const result of results.contents) {
-        console.log(JSON.stringify(result));
+    for(const result in results.contents) {
+        const requestMetrics = results.contents[result];
     }
+
+    await core.summary
+        .addHeading(`Results for ${runName}`)
+        .addTable([
+            [{data: 'Request', header: true}, {data: 'p90', header: true}],
+            ['foo.js', 'Pass ✅'],
+            ['bar.js', 'Fail ❌'],
+            ['test.js', 'Pass ✅']
+        ])
+        .addLink('View staging deployment!', 'https://github.com')
+        .write()
 }
 
 const main = () => {
     // TODO: figure out why input is not registered 
     // const testPath = core.getInput('testPath');
-    const lastRuns = fs.readFileSync(`./test/target/gatling/lastRun.txt`).toString().trim().split('\n');
+    const lastRuns = fs.readFileSync(`${filePath}/gatling/lastRun.txt`).toString().trim().split('\n');
 
     for(const run of lastRuns) {
         generateTestResults(run)
     }
 }
 
-main();
+const generateSummaryTable = (name) => {
+    return await core.summary
+    .addHeading(name)
+}
 
-// await core.summary
-//     .addHeading('Gatling Test Results')
-//     .addCodeBlock(generateTestResults(), "js")
-//     .addTable([
-//         [{data: 'Request', header: true}, {data: 'p90', header: true}],
-//         ['foo.js', 'Pass ✅'],
-//         ['bar.js', 'Fail ❌'],
-//         ['test.js', 'Pass ✅']
-//     ])
-//     .addLink('View staging deployment!', 'https://github.com')
-//     .write()
+main();
 
 
