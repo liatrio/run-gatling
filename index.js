89824ab5511146a326a9fdd6c2b59a40b2829379
Anthony Gauthier <antho325@hotmail.com>
2022-05-13 10:20:43 -0400

    feat: added generateReport.js, added steps in action.yaml for reporting (#3)
    * feat: added generateReport.js, added steps in action.yaml for reporting
    
    * fix: modified job summary step
    
    * chore: fixed indent
    
    * fix: modified download-gatling-report step
    
    * fix: modified Generate CSV report step, added env var
    
    * fix: changed url for gatling-report
    
    * fix: removed trailing )
    
    * fix: downloading v6.0
    
    * refactor: removed gatling-report, using gatling json files
    
    * fix: added @actions/core in package.json
    
    * feat: added compiled index.js
    
    * feat: looping through results
    
    * fix: indent
    
    * fix: added condition to avoid empty newlines in file
    
    * fix: removed package-lock.json, added yarn.lock, added tunnel to dependencies
    
    * feat: attempt at writing summary
    
    * fix: added async keyword before generateTestResults
    
    * feat: added metrics to table
    
    * refactor: added checkmark and 'x' for success/errors
    
    * refactor: using core.getInput for testPath
    
    * fix: assigning testPath in script block rather than index.js
    
    * fix: using env var instead of core.getInput

---
+++ index.js
@@ -0,0 +1,42 @@
+const fs   = require('fs');
+
+module.exports = async ({github, context, core}) => {
+    const testPath = process.env.TEST_PATH
+    const lastRuns = fs.readFileSync(`${testPath}/target/gatling/lastRun.txt`).toString().trim().split('\n');
+
+    for(const run of lastRuns) {
+        const results = JSON.parse(fs.readFileSync(`${testPath}/target/gatling/${run}/js/stats.json`).toString());
+        let tableContent = [
+            [
+                {data: 'Request', header: true}, 
+                {data: 'Success ✅', header: true}, 
+                {data: 'Errors ❌', header: true}, 
+                {data: 'Min', header: true},
+                {data: 'Max', header: true},
+                {data: 'Avg.', header: true},
+                {data: 'Std. Dev.', header: true},
+                {data: 'RPS', header: true},
+            ]
+        ];
+        
+        for(const result in results.contents) {
+            const requestMetrics = results.contents[result].stats;
+            tableContent.push([
+                requestMetrics.name,
+                requestMetrics.numberOfRequests.ok.toString(),
+                requestMetrics.numberOfRequests.ko.toString(),
+                requestMetrics.minResponseTime.total.toString(),
+                requestMetrics.maxResponseTime.total.toString(),
+                requestMetrics.meanResponseTime.total.toString(),
+                requestMetrics.standardDeviation.total.toString(),
+                requestMetrics.meanNumberOfRequestsPerSecond.total.toString(),
+            ]);
+        }
+
+        await core.summary
+            .addHeading(`Results for ${run}`)
+            .addTable(tableContent)
+            .addQuote('All times are in millisecond (ms). RPS means "Requests per Second"')
+            .write()
+    }
+}
